9ba456e07d469ffc8f154880921dd205d0cc1915
Faisal Iqbal <ifaisaliqbal@yahoo.com>
2019-02-21 21:02:11 -0500

    Update simple Regression.py

--- simple Regression.py
+++ simple Regression.py
@@ -10,3 +10,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data = pd.read_csv("Salary_Data.csv");
+
+X = data.iloc[:,:-1].values;
+y = data.iloc[:,1].values;
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2, random_state = 0 )
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
+
+y_pred = regressor.predict(X_test)
